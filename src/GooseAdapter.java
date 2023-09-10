04e3921eb37aba62a5f1b662896aa3965fe19dcd
xMickeyS <pichaya.sen@ku.th>
2023-09-10 13:58:28 +0700

    first commit : Apater Pattern

---
+++ src/GooseAdapter.java
@@ -0,0 +1,10 @@
+public class GooseAdapter implements Quackable {
+    Goose goose;
+    public GooseAdapter(Goose goose) {
+        this.goose = goose;
+    }
+    @Override
+    public void quack() {
+        goose.honk();
+    }
+}
---
+++ src/RedHeadDuck.java
@@ -0,0 +1,6 @@
+public class RedHeadDuck implements Quackable{
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
